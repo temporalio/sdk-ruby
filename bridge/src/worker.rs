62e24e87ab26751101807ff077f6d78d23bbca82
antstorm <antstorm@gmail.com>
2022-07-22 12:10:25 +0100

    Move poll_activity_task_async to a closure

--- bridge/src/worker.rs
+++ bridge/src/worker.rs
@@ -38,25 +38,6 @@ pub enum Response {
 pub type WorkerResult = Result<Vec<u8>, WorkerError>;
 type WorkerCallback = Box<dyn FnOnce(WorkerResult) + Send + 'static>;
 
-async fn poll_activity_task_async(worker: Worker, callback: WorkerCallback) {
-    let task = worker.core_worker.poll_activity_task().await;
-
-    let response = match task {
-        Ok(task) => {
-            let mut bytes: Vec<u8> = Vec::with_capacity(task.encoded_len());
-            task.encode(&mut bytes).expect("Unable to encode activity task protobuf");
-
-            Response::ActivityTask { bytes, callback }
-        },
-        Err(e) => {
-            Response::Error { error: WorkerError::UnableToPollActivityTask(e), callback }
-        }
-    };
-
-    worker.callback_tx.send(response).expect("Unable to send a callback");
-}
-
-#[derive(Clone)]
 pub struct Worker {
     core_worker: Arc<temporal_sdk_core::Worker>,
     tokio_runtime: Arc<TokioRuntime>,
@@ -83,9 +64,27 @@ impl Worker {
     }
 
     pub fn poll_activity_task<F>(&self, callback: F) -> Result<(), WorkerError> where F: FnOnce(WorkerResult) + Send + 'static {
-        self.tokio_runtime.spawn(
-            poll_activity_task_async(self.clone(), Box::new(callback))
-        );
+        let core_worker = self.core_worker.clone();
+        let callback_tx = self.callback_tx.clone();
+        let callback = Box::new(callback);
+
+        self.tokio_runtime.spawn(async move {
+            let task = core_worker.poll_activity_task().await;
+
+            let response = match task {
+                Ok(task) => {
+                    let mut bytes: Vec<u8> = Vec::with_capacity(task.encoded_len());
+                    task.encode(&mut bytes).expect("Unable to encode activity task protobuf");
+
+                    Response::ActivityTask { bytes, callback }
+                },
+                Err(e) => {
+                    Response::Error { error: WorkerError::UnableToPollActivityTask(e), callback }
+                }
+            };
+
+            callback_tx.send(response).expect("Unable to send a callback");
+        });
 
         Ok(())
     }
