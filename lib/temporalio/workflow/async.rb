2f52e74663d00283d7b976d5d6eaf2005e1b5380
antstorm <antstorm@gmail.com>
2023-02-13 13:43:48 +0000

    Amend Async.any behaviour to provide the first completed future

--- lib/temporalio/workflow/async.rb
+++ lib/temporalio/workflow/async.rb
@@ -15,29 +15,29 @@ module Temporalio
         end
       end
 
-      # TODO: Do we need to reject this if any future is rejected?
       def self.all(*futures)
         Future.new do |future, resolve, _reject|
           future.on_cancel { futures.each(&:cancel) }
 
           futures.each do |f|
             f.then do
-              # Resolve the aggregate once all futures have been fulfilled
+              # Resolve the aggregate once all futures have been fulfilled (resolved or rejected)
               resolve.call(nil) if futures.none?(&:pending?)
             end
           end
         end
       end
 
-      # TODO: Do we need to reject this if any future is rejected?
       def self.any(*futures)
+        # This future is never rejected
         Future.new do |future, resolve, _reject|
           future.on_cancel { futures.each(&:cancel) }
 
           futures.each do |f|
-            # Resolve the aggregate once the first future fulfills
-            # All subsequent calls to `resolve` will have no effect
-            f.then { resolve.call(nil) }
+            # Resolve the aggregate once the first future fulfills (resolved or rejected)
+            # NOTE: The the first completed future will be the resolved value and all subsequent
+            #       calls to `resolve` will have no effect.
+            f.then { resolve.call(f) }
           end
         end
       end
