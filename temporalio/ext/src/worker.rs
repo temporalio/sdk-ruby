62ce51619050ca9e24e04c02e31a25d24d75a250
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2026-07-14 10:46:42 -0700

    fix: fmt

--- temporalio/ext/src/worker.rs
+++ temporalio/ext/src/worker.rs
@@ -569,9 +569,7 @@ fn build_config(options: Struct, runtime_handle: &RuntimeHandle) -> Result<Worke
                 })
                 .collect::<HashSet<_>>(),
         )
-        .disable_payload_error_limit(
-            options.member::<bool>(id!("disable_payload_error_limit"))?,
-        )
+        .disable_payload_error_limit(options.member::<bool>(id!("disable_payload_error_limit"))?)
         .build()
         .map_err(|err| error!("Invalid worker options: {}", err))
 }
