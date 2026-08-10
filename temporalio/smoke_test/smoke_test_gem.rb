c85b6d3488281f3ffe041f85a5cbb784c17943c2
Greg Travis <greg.m.travis@gmail.com>
2026-08-10 12:32:41 -0400

    Share code between pre- and post-release smoke tests

--- temporalio/smoke_test/smoke_test_gem.rb
+++ temporalio/smoke_test/smoke_test_gem.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
-raise 'Must provide gem file glob' if ARGV.length != 1
+# Smoke test for a locally-built or published temporalio gem.
+#
+#   ruby smoke_test_gem.rb GEM_GLOB
+#     Install a local .gem file matched by the glob.
+#
+#   ruby smoke_test_gem.rb --version VERSION
+#     Install temporalio at VERSION from rubygems.org.
 
-gem_files = Dir.glob(ARGV.first)
-raise "Unable to find single gem file, found #{gem_files.length}" unless gem_files.length == 1
+require 'optparse'
+
+options = { version: nil }
+parser = OptionParser.new do |o|
+  o.banner = "Usage: smoke_test_gem.rb GEM_GLOB\n" \
+             "   or: smoke_test_gem.rb --version VERSION"
+  o.on('--version V', 'Install temporalio VERSION from rubygems.org (instead of a local .gem)') { |v| options[:version] = v }
+end
+positional = parser.parse(ARGV)
 
 # TODO(cretz): For Linux musl, we have to install google-protobuf manually because latest versions do not work with
 # musl. Remove this when https://github.com/protocolbuffers/protobuf/issues/16853 is resolved.
@@ -11,7 +24,18 @@ if RUBY_PLATFORM.include?('linux-musl')
   system('gem', 'install', '--verbose', 'google-protobuf', '--platform', 'ruby', exception: true)
 end
 
-system('gem', 'install', '--verbose', gem_files.first, exception: true)
+if options[:version]
+  raise 'Unexpected positional arguments in --version mode' unless positional.empty?
+
+  system('gem', 'install', '--verbose', 'temporalio', '-v', options[:version], exception: true)
+else
+  raise parser.help if positional.length != 1
+
+  gem_files = Dir.glob(positional.first)
+  raise "Unable to find single gem file, found #{gem_files.length}" unless gem_files.length == 1
+
+  system('gem', 'install', '--verbose', gem_files.first, exception: true)
+end
 
 # Create a local environment and start a workflow
 require 'temporalio/client'
